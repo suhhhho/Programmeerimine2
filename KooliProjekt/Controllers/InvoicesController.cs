d77ad56ac10ffe81dfb1b15b60f5fd7dac44dc2a
Jasper Tetsmann <Jasper.Tetsmann@tptlive.ee>
2024-10-18 15:35:21 +0300

    git commit -a

---
+++ KooliProjekt/Controllers/InvoicesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KooliProjekt.Data;
+
+namespace KooliProjekt.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvoicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Invoices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Invoices.ToListAsync());
+        }
+
+        // GET: Invoices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,InvoiceNo,InvoiceDate,DueDate")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(invoice);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,InvoiceNo,InvoiceDate,DueDate")] Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invoice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceExists(invoice.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice != null)
+            {
+                _context.Invoices.Remove(invoice);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvoiceExists(int id)
+        {
+            return _context.Invoices.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ KooliProjekt/Controllers/RentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KooliProjekt.Data;
+
+namespace KooliProjekt.Controllers
+{
+    public class RentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Rent.Include(r => r.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Rents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rent = await _context.Rent
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rent == null)
+            {
+                return NotFound();
+            }
+
+            return View(rent);
+        }
+
+        // GET: Rents/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Rents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,śtart_time,end_time,kilometrs_driven,is_cancelled,carId,UserId")] Rent rent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rent.UserId);
+            return View(rent);
+        }
+
+        // GET: Rents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rent = await _context.Rent.FindAsync(id);
+            if (rent == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rent.UserId);
+            return View(rent);
+        }
+
+        // POST: Rents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,śtart_time,end_time,kilometrs_driven,is_cancelled,carId,UserId")] Rent rent)
+        {
+            if (id != rent.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentExists(rent.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", rent.UserId);
+            return View(rent);
+        }
+
+        // GET: Rents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rent = await _context.Rent
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rent == null)
+            {
+                return NotFound();
+            }
+
+            return View(rent);
+        }
+
+        // POST: Rents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rent = await _context.Rent.FindAsync(id);
+            if (rent != null)
+            {
+                _context.Rent.Remove(rent);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentExists(int id)
+        {
+            return _context.Rent.Any(e => e.Id == id);
+        }
+    }
+}
